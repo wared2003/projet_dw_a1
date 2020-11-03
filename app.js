fcfdffa3caa8ba5066b670a0795fe584cdd2fab0
Edouard Nicolas <edouardn809@gmail.com>
2020-11-03 18:55:52 +0100

    rendu prof mardi soir

--- app.js
+++ app.js
@@ -12,4 +12,14 @@ $(document).ready(function(){
         $('.banner_no button').css({'display' : 'inline-block'});
     });
 
+    var mymap = L.map('mapid').setView([48.8566969, 2.3514616], 13);
+
+    L.tileLayer('https://api.mapbox.com/styles/v1/{id}/tiles/{z}/{x}/{y}?access_token={accessToken}', {
+    attribution: 'Map data &copy; <a href="https://www.openstreetmap.org/">OpenStreetMap</a> contributors, <a href="https://creativecommons.org/licenses/by-sa/2.0/">CC-BY-SA</a>, Imagery © <a href="https://www.mapbox.com/">Mapbox</a>',
+    maxZoom: 18,
+    id: 'mapbox/streets-v11',
+    tileSize: 512,
+    zoomOffset: -1,
+    accessToken: 'pk.eyJ1Ijoid2FyZWQyMDAzIiwiYSI6ImNqeWQxNWFkdDBueWUzY3A4dTdlZTI3OG0ifQ.loi4SD_6Y0LrRSa04zryXg'
+}).addTo(mymap);
 });
