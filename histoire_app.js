08da2fd7cde5aa41d99c140b53864825c83392ab
Edouard Nicolas <edouardn809@gmail.com>
2020-11-15 00:32:23 +0100

    ajout validation continu form ! encor un bug sur la text area

--- histoire_app.js
+++ histoire_app.js
@@ -10,7 +10,7 @@ $(document).ready(function(){
         sections.push($($(this).attr('href')));
     });
 
-    $(window).scroll(function(e){
+    $(window).scroll(function(){
         var scrollTop = $(this).scrollTop() + ($(window).height()) / 2;
         for(var i in sections){
             var section = sections[i];
