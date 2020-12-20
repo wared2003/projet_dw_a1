b25bbf5a421d41ff33b954071f77ae76fda044db
Edouard Nicolas <edouardn809@gmail.com>
2020-12-20 15:38:05 +0100

    ajout commentaire slider dans la galerie

--- js/galerie_app.js
+++ js/galerie_app.js
@@ -23,10 +23,47 @@ $(document).ready(function(){
         galerie.masonry('layout');
     });
 
-    galerie.find('.galerie_bloc').click(function(){
+    galerie.find('.galerie_img').click(function(){
         element = this;
-        $('.galerie_bloc').not(element).removeClass('big')
-        $(element).toggleClass('big');
-        galerie.masonry('layout')
+        com_slider_position = 0
+        $('.galerie_bloc').not(element).parent().removeClass('big');
+        $('.galerie_bloc').not(element).parent().children().removeClass('visible');
+        $(element).parent().toggleClass('big');
+        $(element).parent().children().toggleClass('visible');
+        galerie.masonry('layout');
     })
+
+    //caroussel affichage des commetaires
+
+    //element suivant
+    $('.next').click(function(){
+        var coms = $(element).parent().find('.img_com')
+        var curent = $('.active');
+        var next = curent.next()
+        console.log(coms.length);
+
+        if(com_slider_position < (coms.length - 1)){
+            curent.removeClass('active').css('z-index', -10);
+            next.addClass('active').css('z-index', 10);
+            next.length = 0;
+            com_slider_position ++ ;
+        };
+    });
+
+    //element précédent
+    $('.previous').click(function(){
+        var curent = $('.active');
+        var prev = curent.prev();
+
+        if(com_slider_position > 0){
+            curent.removeClass('active').css('z-index', -10);
+            prev.addClass('active').css('z-index', 10);
+            com_slider_position -- ;
+        };
+    });
+
+
+
+
+
 });
